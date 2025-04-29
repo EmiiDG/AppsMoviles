88f05bc79a2f86842a2f7d047c548dc815c45abd
Emiliano Di Grappa <emilianodigrappa@gmail.com>
2025-04-29 13:04:48 -0300

    Añadir ojo

--- app/src/main/java/com/appsmoviles/tp1/HomeActivity.kt
+++ app/src/main/java/com/appsmoviles/tp1/HomeActivity.kt
@@ -10,12 +10,15 @@ import android.content.Intent
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.text.InputType
+import android.widget.ImageButton
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.primerapagina)
+        var contraseña = findViewById<EditText>(R.id.editTextTextPassword2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,11 +32,25 @@ class HomeActivity : AppCompatActivity() {
 
             val ingresar = findViewById<Button>(R.id.button3)
             ingresar.setOnClickListener{
-                if(findViewById<EditText>(R.id.editTextText).text.toString() == "Juan Torres" && findViewById<EditText>(R.id.editTextTextPassword2).text.toString() == "1234utn"){
+                if(findViewById<EditText>(R.id.editTextText).text.toString() == "Juan Torres" && contraseña.text.toString() == "1234utn"){
                 val intent2 = Intent(this, bienvenidaActivity::class.java)
                 startActivity(intent2)}else{
                     findViewById<TextView>(R.id.textView2).visibility = View.VISIBLE
                 }
             }
+        val toggleBtn3 = findViewById<ImageButton>(R.id.btnTogglePassword3)
+        var isVisible3 = false
+        toggleBtn3.setOnClickListener {
+
+            if (isVisible3) {
+                contraseña.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                toggleBtn3.setImageResource(R.drawable.ic_eye_closed)
+            } else {
+                contraseña.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                toggleBtn3.setImageResource(R.drawable.ic_eye_open)
+            }
+            contraseña.setSelection(contraseña.text.length)
+            isVisible3 = !isVisible3
+        }
     }
 }
--- app/src/main/java/com/appsmoviles/tp1/RegistroActivity.kt
+++ app/src/main/java/com/appsmoviles/tp1/RegistroActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.text.InputType
 
 class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,31 +25,38 @@ class RegistroActivity : AppCompatActivity() {
         }
         val boton = findViewById<Button>(R.id.button2)
 
+        val contraseña = findViewById<EditText>(R.id.editTextTextPassword)
+        val confirmar = findViewById<EditText>(R.id.editTextTextPassword3)
         boton.setOnClickListener{
             val nombre = findViewById<EditText>(R.id.editTextText2)
             val mail = findViewById<EditText>(R.id.editTextTextEmailAddress)
-            val contraseña = findViewById<EditText>(R.id.editTextTextPassword)
-            val confirmar = findViewById<EditText>(R.id.editTextTextPassword3)
             val texton = findViewById<TextView>(R.id.textView)
             val textom = findViewById<TextView>(R.id.textView3)
             val textoc = findViewById<TextView>(R.id.textView5)
             val textocc = findViewById<TextView>(R.id.textView4)
 
-            if (nombre.text.toString().isEmpty()){
+            if (nombre.text.toString().isEmpty() || nombre.text.toString().length < 2 || !nombre.text.toString().matches(Regex("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+\$"))) {
                 texton.visibility = View.VISIBLE
-            }else{
+                texton.text = "El nombre no es válido"
+            } else {
                 texton.visibility = View.GONE
             }
 
+
             if (mail.text.toString().isEmpty()){
                 textom.visibility = View.VISIBLE
+                textom.text="El campo email no puede estar vacío"
+            }else if(!isValidEmail(mail.text.toString())){
+                textom.visibility = View.VISIBLE
+                textom.text = "El email no es válido"
             }else{
                 textom.visibility = View.GONE
             }
 
-            if (contraseña.text.toString().length < 6){
+            if (!isValidPassword(contraseña.text.toString())) {
                 textoc.visibility = View.VISIBLE
-            }else{
+                textoc.text = "La contraseña debe tener al menos 6 caracteres, una mayúscula, una minúscula, un número y un símbolo"
+            } else {
                 textoc.visibility = View.GONE
             }
 
@@ -61,5 +70,45 @@ class RegistroActivity : AppCompatActivity() {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)}
         }
+
+        val toggleBtn = findViewById<ImageButton>(R.id.btnTogglePassword)
+        var isVisible = false
+        toggleBtn.setOnClickListener {
+
+            if (isVisible) {
+                contraseña.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                toggleBtn.setImageResource(R.drawable.ic_eye_closed)
+            } else {
+                contraseña.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                toggleBtn.setImageResource(R.drawable.ic_eye_open)
+            }
+            contraseña.setSelection(contraseña.text.length)
+            isVisible = !isVisible
+        }
+
+        val toggleBtn2 = findViewById<ImageButton>(R.id.btnTogglePassword2)
+        var isVisible2 = false
+        toggleBtn2.setOnClickListener {
+
+            if (isVisible2) {
+                confirmar.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                toggleBtn2.setImageResource(R.drawable.ic_eye_closed)
+            } else {
+                confirmar.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                toggleBtn2.setImageResource(R.drawable.ic_eye_open)
+            }
+            confirmar.setSelection(contraseña.text.length)
+            isVisible2 = !isVisible2
+        }
     }
+}
+
+fun isValidEmail(email: String): Boolean {
+    val emailRegex = Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+    return emailRegex.matches(email)
+}
+
+fun isValidPassword(password: String): Boolean {
+    val passwordRegex = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#\$%^&+=!.,]).{6,}\$")
+    return passwordRegex.matches(password)
 }
